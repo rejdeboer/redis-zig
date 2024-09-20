e8a130bba28e3a4fd4da5dc88d8414b0ca843496
rejdeboer <rick.deboer@live.nl>
2024-09-20 17:03:59 +0200

    feat: finish client ping function

--- src/client/client.zig
+++ src/client/client.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
 const net = std.net;
 
+// Note: This client is not thread-safe
 const Redis = struct {
     stream: net.Stream,
+    buffer: [1024]u8,
 
     pub fn connect(host: []const u8, port: u16) !Redis {
         const address = try net.Address.parseIp4(host, port);
@@ -10,13 +12,17 @@ const Redis = struct {
 
         return Redis{
             .stream = stream,
+            .buffer = undefined,
         };
     }
 
-    pub fn ping(self: Redis) !void {
+    pub fn ping(self: Redis) !bool {
         const writer = self.stream.writer();
-        const _reader = self.stream.reader();
+        const reader = self.stream.reader();
         try writer.writeAll("+PING\r\n");
+        const bytes_read = try reader.readAll(&self.buffer);
+        const msg = self.buffer[0..bytes_read];
+        return std.mem.eql(u8, "+PONG\r\n", msg);
     }
 
     pub fn close(self: Redis) void {
