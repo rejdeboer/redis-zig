7038df50158e2b54c239a82e3d7dd338ab424d48
rejdeboer <rick.deboer@live.nl>
2024-09-27 23:18:50 +0200

    fix: use correct function to parse int

--- lib/parser/parser.zig
+++ lib/parser/parser.zig
@@ -15,12 +15,16 @@ pub const ParsingError = error{Unexpected};
 
 pub fn parse(message: []const u8) Command {
     var tokens = std.mem.tokenizeSequence(u8, message, "\r\n");
-    _ = try parse_list_length(tokens.next());
+    _ = try parse_list_length(tokens.next().?);
+
+    while (tokens.next()) |line| {
+        switch (line[0]) {}
+    }
 }
 
 fn parse_list_length(line: []const u8) !u32 {
-    if (!std.mem.eql(u8, '*', line[0])) {
+    if ('*' != line[0]) {
         return ParsingError.Unexpected;
     }
-    return std.fmt.formatInt(u32, line[1..], 10) orelse ParsingError.Unexpected;
+    return std.fmt.parseInt(u32, line[1..], 10) orelse ParsingError.Unexpected;
 }
