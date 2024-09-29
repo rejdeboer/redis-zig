f0317adf8fc000e4bf4ace111dd651ebde411644
rejdeboer <rick.deboer@live.nl>
2024-09-29 23:06:45 +0200

    feat: handle ping and echo in parser

--- lib/parser/parser.zig
+++ lib/parser/parser.zig
@@ -15,16 +15,32 @@ pub const ParsingError = error{Unexpected};
 
 pub fn parse(message: []const u8) Command {
     var tokens = std.mem.tokenizeSequence(u8, message, "\r\n");
-    _ = try parse_list_length(tokens.next().?);
+    const command_length = try parse_list_length(tokens.next().?);
 
-    while (tokens.next()) |line| {
-        switch (line[0]) {}
+    try expect_char(tokens.next(), '$');
+    const command = tokens.next();
+
+    if (std.ascii.eqlIgnoreCase("PING", command)) {
+        if (command_length > 1) {
+            try expect_char(tokens.next(), '$');
+            return Command{ .ping = tokens.next() };
+        }
+        return Command{ .ping = null };
+    } else if (std.ascii.eqlIgnoreCase("ECHO", command)) {
+        try expect_char(tokens.next(), '$');
+        return Command{ .echo = tokens.next() };
     }
+    return ParsingError.Unexpected;
 }
 
 fn parse_list_length(line: []const u8) !u32 {
-    if ('*' != line[0]) {
+    try expect_char(line, '*');
+    return std.fmt.parseInt(u32, line[1..], 10) orelse ParsingError.Unexpected;
+}
+
+fn expect_char(line: []const u8, char: u8) !void {
+    if (char != line[0]) {
+        std.log.info("parsing error: expected {}, received {}", .{ char, line[0] });
         return ParsingError.Unexpected;
     }
-    return std.fmt.parseInt(u32, line[1..], 10) orelse ParsingError.Unexpected;
 }
