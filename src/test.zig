d16265d22d2b3111c47e6f9e0ff40477773a4d71
rejdeboer <rick.deboer@live.nl>
2024-10-01 22:25:06 +0200

    feat: get set test case

--- src/test.zig
+++ src/test.zig
@@ -25,3 +25,13 @@ test "echo" {
     const msg = try redis.send([]const u8, "*2\r\n$4\r\nECHO\r\n$4\r\ntest\r\n");
     try std.testing.expect(std.mem.eql(u8, "test", msg));
 }
+
+test "set get" {
+    try start_test_server();
+    var redis = try client.Redis.connect("127.0.0.1", 6379);
+    defer redis.close();
+    const set_response = try redis.send([]const u8, "*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n");
+    try std.testing.expect(std.mem.eql(u8, "OK", set_response));
+    const get_response = try redis.send([]const u8, "*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n");
+    try std.testing.expect(std.mem.eql(u8, "bar", get_response));
+}
