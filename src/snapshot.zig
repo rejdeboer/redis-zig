c7c2a9620d96bc75be5da3b60726213f5af73f02
rejdeboer <rick.deboer@live.nl>
2024-12-11 22:50:07 +0100

    feat: add constants for snapshot

--- src/snapshot.zig
+++ src/snapshot.zig
@@ -5,9 +5,15 @@ const RedisEntry = @import("parser.zig").RedisEntry;
 const REDIS_HEADER: []const u8 = "REDIS0011";
 const REDIS_VERSION: []const u8 = "redis-ver6.0.16";
 
-const METADATA_START: u8 = 0xFA;
-const DATABASE_START: u8 = 0xFE;
-const EOF_FLAG: u8 = 0xFF;
+const OPCODE_AUX: u8 = 0xFA;
+const OPCODE_RESIZE_DB: u8 = 0xFB;
+const OPCODE_EXPIRE_TIME_MS: u8 = 0xFC;
+const OPCODE_EXPIRE_TIME: u8 = 0xFD;
+const OPCODE_SELECT_DB: u8 = 0xFE;
+const OPCODE_EOF: u8 = 0xFF;
+
+const TYPE_STRING: u8 = 0x00;
+const TYPE_LIST: u8 = 0x01;
 
 pub fn store_snapshot(allocator: std.mem.Allocator, path: []const u8, file_name: []const u8, storage: std.StringHashMap(RedisEntry)) !void {
     const file_path = try std.mem.concat(allocator, u8, .{ path, file_name });
